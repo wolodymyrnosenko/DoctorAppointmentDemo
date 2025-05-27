b88c6f9829974c24f3a4647a59884dec69858bd1
volodymyr <vovanose@ukr.net>
2025-05-27 16:04:04 +0300

    Add AppointmentService

---
+++ DoctorAppointmentDemo.Service/Services/AppointmentService.cs
@@ -0,0 +1,42 @@
+﻿using MyDoctorAppointment.Data.Interfaces;
+using MyDoctorAppointment.Data.Repositories;
+using MyDoctorAppointment.Domain.Entities;
+using MyDoctorAppointment.Service.Interfaces;
+
+namespace MyDoctorAppointment.Service.Services
+{
+    public class AppointmentService : IAppointmentService
+    {
+        private readonly IAppointmentRepository _appointmentRepository;
+
+        public AppointmentService()
+        {
+            _appointmentRepository = new AppointmentRepository();
+        }
+
+        public Appointment Create(Appointment appointment)
+        {
+            return _appointmentRepository.Create(appointment);
+        }
+
+        public bool Delete(int id)
+        {
+            return _appointmentRepository.Delete(id);
+        }
+
+        public Appointment? Get(int id)
+        {
+            return _appointmentRepository.GetById(id);
+        }
+
+        public IEnumerable<Appointment> GetAll()
+        {
+            return _appointmentRepository.GetAll();
+        }
+
+        public Appointment Update(int id, Appointment appointment)
+        {
+            return _appointmentRepository.Update(id, appointment);
+        }
+    }
+}
