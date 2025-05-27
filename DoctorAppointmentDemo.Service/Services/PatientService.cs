527c8a6c387b9816bf3f97ff217f4368f8d53f0c
volodymyr <vovanose@ukr.net>
2025-05-27 10:44:40 +0300

    Add PatientService

---
+++ DoctorAppointmentDemo.Service/Services/PatientService.cs
@@ -0,0 +1,42 @@
+﻿using MyDoctorAppointment.Data.Interfaces;
+using MyDoctorAppointment.Data.Repositories;
+using MyDoctorAppointment.Domain.Entities;
+using MyDoctorAppointment.Service.Interfaces;
+
+namespace MyDoctorAppointment.Service.Services
+{
+    public class PatientService : IPatientService
+    {
+        private readonly IPatientRepository _patientRepository;
+
+        public PatientService()
+        {
+            _patientRepository = new PatientRepository();
+        }
+
+        public Patient Create(Patient patient)
+        {
+            return _patientRepository.Create(patient);
+        }
+
+        public bool Delete(int id)
+        {
+            return _patientRepository.Delete(id);
+        }
+
+        public Patient? Get(int id)
+        {
+            return _patientRepository.GetById(id);
+        }
+
+        public IEnumerable<Patient> GetAll()
+        {
+            return _patientRepository.GetAll();
+        }
+
+        public Patient Update(int id, Patient patient)
+        {
+            return _patientRepository.Update(id, patient);
+        }
+    }
+}
