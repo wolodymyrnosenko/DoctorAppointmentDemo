d9ec59de54629c773194e7752af1b056d0c173e2
volodymyr <vovanose@ukr.net>
2025-05-27 15:58:24 +0300

    Add IAppointmentService

---
+++ DoctorAppointmentDemo.Service/Interfaces/IAppointmentService.cs
@@ -0,0 +1,17 @@
+﻿using MyDoctorAppointment.Domain.Entities;
+
+namespace MyDoctorAppointment.Service.Interfaces
+{
+    public interface IAppointmentService
+    {
+        Appointment Create(Appointment appointment);
+
+        IEnumerable<Appointment> GetAll();
+
+        Appointment? Get(int id);
+
+        bool Delete(int id);
+
+        Appointment Update(int id, Appointment appointment);
+    }
+}
