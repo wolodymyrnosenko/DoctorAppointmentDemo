65ae387a192e9609a546fd579980b37e8e112420
volodymyr <vovanose@ukr.net>
2025-05-27 11:12:12 +0300

    Test in Program CRUD Patient

--- DoctorAppointmentDemo.UI/Program.cs
+++ DoctorAppointmentDemo.UI/Program.cs
@@ -7,10 +7,12 @@ namespace MyDoctorAppointment
     public class DoctorAppointment
     {
         private readonly IDoctorService _doctorService;
+        private readonly IPatientService _patientService;
 
         public DoctorAppointment()
         {
             _doctorService = new DoctorService();
+            _patientService = new PatientService();
         }
 
         public void Menu()
@@ -20,33 +22,58 @@ namespace MyDoctorAppointment
             //    // add Enum for menu items and describe menu
             //}
 
+
+            Console.WriteLine("Current patients list: ");
+            foreach(var patient in _patientService.GetAll())
+                Console.WriteLine($"ID: {patient.Id} {patient.Name}");
+
+            Console.WriteLine("Adding patient: ");
+
+            var newPatient = new Patient
+            {
+                Name = "Mykola",
+                Surname = "Hvoryi",
+            };
+
+            _patientService.Create(newPatient);
+
+            Console.WriteLine("Current patients list: ");
+            foreach (var patient in _patientService.GetAll())
+                Console.WriteLine($"ID: {patient.Id} {patient.Name}");
+
+            Console.WriteLine("Delete patient: ");
+            _patientService.Delete(newPatient.Id);
+            Console.WriteLine("Current patients list: ");
+            foreach (var patient in _patientService.GetAll())
+                Console.WriteLine($"ID: {patient.Id} {patient.Name}");
+
             Console.WriteLine("Current doctors list: ");
             var docs = _doctorService.GetAll();
 
-            foreach (var doc in docs)
-            {
-                Console.WriteLine(doc.Name);
-            }
+            //foreach (var doc in docs)
+            //{
+            //    Console.WriteLine(doc.Name);
+            //}
 
-            Console.WriteLine("Adding doctor: ");
+            //Console.WriteLine("Adding doctor: ");
 
-            var newDoctor = new Doctor
-            {
-                Name = "Vasya",
-                Surname = "Petrov",
-                Experience = 20,
-                DoctorType = Domain.Enums.DoctorTypes.Dentist
-            };
+            //var newDoctor = new Doctor
+            //{
+            //    Name = "Vasya",
+            //    Surname = "Petrov",
+            //    Experience = 20,
+            //    DoctorType = Domain.Enums.DoctorTypes.Dentist
+            //};
 
-            //_doctorService.Create(newDoctor);
+            ////_doctorService.Create(newDoctor);
 
-            Console.WriteLine("Current doctors list: ");
-            docs = _doctorService.GetAll();
+            //Console.WriteLine("Current doctors list: ");
+            //docs = _doctorService.GetAll();
 
-            foreach (var doc in docs)
-            {
-                Console.WriteLine(doc.Name);
-            }
+            //foreach (var doc in docs)
+            //{
+            //    Console.WriteLine(doc.Name);
+            //}
         }
     }
 
